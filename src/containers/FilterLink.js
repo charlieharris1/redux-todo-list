f5e0bf2f73bcf983c57a09ba399ac379e9c8ebd1
Charlie Harris <ceharris92@gmail.com>
2019-02-02 17:08:53 +0000

    Complete the todo list

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux'
-// import { setVisibilityFilter } from '../actions'
+import { setVisibilityFilter } from '../actions'
 import Link from '../components/Link'
 
 const mapStateToProps = (state, ownProps) => ({
@@ -7,7 +7,7 @@ const mapStateToProps = (state, ownProps) => ({
 })
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  // onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
+  onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
 })
 
 export default connect(
--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux'
-// import { toggleTodo } from '../actions'
+import { toggleTodo } from '../actions'
 import TodoList from '../components/TodoList'
 import { VisibilityFilters } from '../actions'
 
@@ -21,7 +21,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  // toggleTodo: id => dispatch(toggleTodo(id))
+  toggleTodo: id => dispatch(toggleTodo(id))
 })
 
 export default connect(
